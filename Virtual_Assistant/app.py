dd32a7107a806583ca1e99222ceaf530512b7c5f
vaibhavsharma <vaibhav53091@gmail.com>
2026-09-24 19:43:35 +0530

    feat: add Google speech recognition

--- Virtual_Assistant/app.py
+++ Virtual_Assistant/app.py
@@ -13,3 +13,23 @@ def speak(text):
 if __name__ == "__main__":
     speak("initializing ultron.....")
     
+    while True:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+            
+        try:
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+
+        
+
+      
+
+    
+
