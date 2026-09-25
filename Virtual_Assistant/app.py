7979fce5606f78450a1f7bb490e7ea2c73edea9c
vaibhavsharma <vaibhav53091@gmail.com>
2026-09-25 08:09:51 +0530

    feat: add Google browser command

--- Virtual_Assistant/app.py
+++ Virtual_Assistant/app.py
@@ -10,22 +10,44 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open('https://google.com')
+        
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+
+    elif "open github" in c.lower():
+        webbrowser.open("https://github.com")
+    
 if __name__ == "__main__":
     speak("initializing ultron.....")
     
     while True:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Say something!")
-            audio = r.listen(source)
-            
+        r = sr.Recognizer()      
+        
         try:
-            command = r.recognize_google(audio)
-            print(command)
+            with sr.Microphone() as source:
+                print("Listening.....")
+                audio = r.listen(source,timeout=2,phrase_time_limit=1)
+                print("recognizing...")  
+            word = r.recognize_google(audio)
+            if(word.lower()=="ultron"):
+                speak("ya")
+                
+                with sr.Microphone() as source:
+                    print("Ultron Active")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    
+                    processcommand(command)
+                    
+        except sr.WaitTimeoutError:
+            print("Listening timed out")     
         except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
+            print("i dont understand")
         except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
+            print("ultron error; {0}".format(e))
 
         
 
